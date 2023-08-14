6cb1a01d01f78176132799a3d863eaeab5c1b380
mar4587 <marshetnigatu@yahoo.com>
2023-08-14 01:49:32 -0700

    Inventing is a combination of brains and materials

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit numbers of base 10,,
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0 ; a < 9 ; a++)
+	{
+		for (b = a + 1; b < 10; b++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+		if (a == 8 && b == 9)
+			continue;
+	putchar(',');
+	putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
