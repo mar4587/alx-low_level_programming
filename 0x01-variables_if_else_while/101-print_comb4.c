851e1e46cdb7cb0d43a0ed951c4f1ce23f0cf939
mar4587 <marshetnigatu@yahoo.com>
2023-08-14 02:05:18 -0700

    The success combination in business is: Do what you do better

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit numbers of base 10,,
+ * followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0 ; a < 8 ; a++)
+	{
+		for (b = a + 1; b < 9; b++)
+		{
+			for (c = b + 1; b < 10; c++)
+			{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+			putchar((c % 10) + '0');
+		if (a == 7 && b == 8 && c == 9)
+			continue;
+	putchar(',');
+	putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
