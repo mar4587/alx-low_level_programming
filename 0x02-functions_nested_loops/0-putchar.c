5b00e0083f57e3dc343ecf7031f9464b004a17af
mar4587 <marshetnigatu@yahoo.com>
2023-08-15 06:28:03 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - this program prints _putchar
+ *Return 0 (Success)
+ */
+int main(void)
+{
+	int j;
+	char c[] = "_putchar";
+
+	for (j = 0; j <= 8; j++)
+	{
+		_putchar(c[j]);
+	}
+	_putchar('\n');
+	return (0);
+}
