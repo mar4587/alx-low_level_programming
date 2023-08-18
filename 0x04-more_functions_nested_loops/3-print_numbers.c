80341841ccf8f1bec62252f9fe87c4722a6a294e
mar4587 <marshetnigatu@yahoo.com>
2023-08-18 00:46:22 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,17 @@
-include "main.h"
+#include "main.h"
 /**
  * print_numbers - print 0-9
  * return: void
+ *
  */
-void print_ numbers(void)
+
+void print_numbers(void)
 {
-	char c;
-	for (c = '0'; c <= '9'; c++)
+	int c;
+
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(c)
+		_putchar(c + '0');
 	}
 	_putchar('\n');
 }
