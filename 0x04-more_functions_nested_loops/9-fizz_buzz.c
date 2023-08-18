1d257fe1dc466d6696d1dc63338c315bf25f9bb1
mar4587 <marshetnigatu@yahoo.com>
2023-08-18 02:25:24 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	{
 		if ((a % 3) == 0 && (a % 5) == 0)
 		{
-			printf("FizzBuzz ");
+		printf("FizzBuzz ");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+		printf("Fizz ");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+		printf("Buzz ");
 		}
 		else
 		{
@@ -32,4 +32,3 @@ int main(void)
 
 	return (0);
 }
-
