1336b1e275b19f96fd60d6e5390ed6848ad7559a
mar4587 <marshetnigatu@yahoo.com>
2023-08-21 06:34:07 -0700

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * print_rev - This  prints a string, in reverse, followed by a new line
  *
@@ -10,9 +11,9 @@ void print_rev(char *s)
 {
 	int x;
 
-	for (x = strlen[s] - 1; x >= 0; x--)
+	for (x = strlen(s) - 1; x >= 0; x--)
 	{
-		_putchare(s[x]);
+		_putchar(s[x]);
 	}
 	_putchar('\n');
 }
