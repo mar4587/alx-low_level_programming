0f74f1c53145d8e1c78ec3a5f768d1fe5f3f23ec
mar4587 <marshetnigatu@yahoo.com>
2023-08-24 05:09:56 -0700

    Task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - This compares
+ * @s1: first s
+ * @s2: second s
+ *
+ * Return: a
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = strcmp(s1, s2);
+
+	return (a);
+}
