e4a30a9935aa1b5569f6f963f9698c3d031811b4
mar4587 <marshetnigatu@yahoo.com>
2023-08-24 04:58:45 -0700

    Task2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,10 +12,19 @@ char *_strncat(char *dest, char *src, int n)
 	int ld, ls, a;
 
 	for (ld = 0; dest[ld] != '\0'; ld++)
-	for (ls = 0; src[ls] != '\0'; ls++)
-	for (a = 0; a < n; a++)
 	{
-		dest[ld + a] = src[a]
+
+	}
+	for (ls = 0; src[ls] != '\0'; ls++)
+	{
+
+	}
+	for (a = 0; a <= ls; a++)
+	{
+		if (a < n)
+		{
+			dest[ld + a] = src[a];
+		}
 	}
 	return (dest);
 }
