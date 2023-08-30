709e124d5b01220b563464d076490affac7431f1
mar4587 <marshetnigatu@yahoo.com>
2023-08-30 13:00:21 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * _puts_recursion - recursive function to print to screen
- *
- * @s: pointer to the string
- *
- * Return: nothing
- */
+* _puts_recursion - recursive function to print to screen
+* @s: pointer to the string
+* Return: nothing
+*/
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
@@ -13,27 +11,8 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	if (*s == '\0')
+		(*s == '\0')
 	{
 		_putchar('\n');
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar -the locked away a secret, deep inside herself, something she once knew to be true
+ * @c: to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (write(1, & c, 1));
 }
