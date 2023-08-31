3ba0bb9a71fd2d59bee25587b3ce0c625e7a7f07
mar4587 <marshetnigatu@yahoo.com>
2023-08-31 04:39:35 -0700

    She locked away a secret, deep inside herself

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,16 @@
 /**
 * _puts_recursion - recursive function to print to screen
 * @s: pointer to the string
-* Return: nothing
+*
 */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		_putchar(*s)
 		_puts_recursion(s + 1);
 	}
-		(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
