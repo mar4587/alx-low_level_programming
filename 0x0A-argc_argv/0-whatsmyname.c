13e6e8b1c2e400074c058b2fe6e78dab59105bf8
mar4587 <marshetnigatu@yahoo.com>
2023-09-01 13:36:51 -0700

    tas0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This function prints its name
+ * @argc: number of the command line
+ * @argv: the array
+ * Return: 0 (Successful)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this program prints its name, followed by a new line
+ *
+ * argc: number of command line
+ * argv: array contain line arg ..
+ * Return (0)
+ */
+int main (int argc, char *argv[])
+{
+	printf("%s\n", 0, argv[0]);
+	Return 0;
+}
