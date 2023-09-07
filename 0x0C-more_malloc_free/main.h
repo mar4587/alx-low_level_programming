d318ed293d88586fb9eb870ea8f97e754596cf43
mar4587 <marshetnigatu@yahoo.com>
2023-09-07 06:55:52 -0700

    task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,9 @@
-#endif 
-#define
-malloc_checked(unsigned int b);
+#ifndef _MAIN _H_
+#define _MAIN _H_
+
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+
 #endif
