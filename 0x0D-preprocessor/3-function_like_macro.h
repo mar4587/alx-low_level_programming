a87651f56e8e7b70c7edc8dbee66b122cb0b9631
mar4587 <marshetnigatu@yahoo.com>
2023-09-11 14:37:43 -0700

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ * File: 3-function_like_macro.h
+ *
+ * @x: input
+ *
+ * Desc: header file
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
