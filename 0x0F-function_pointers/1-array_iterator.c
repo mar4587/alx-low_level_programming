5bb3875f82674f10a42d86339877d241b83a0832
mar4587 <marshetnigatu@yahoo.com>
2023-09-13 07:16:00 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - This iterates through an array
+ * @array: target 
+ * @size: the size
+ * @action: function pointer
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int y;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (y = 0; y < size; y++)
+	{
+		action(*array);
+		array++;
+	}
+}
