61840405aba633c54a0aea2ab27b0d1531a609dd
mar4587 <marshetnigatu@yahoo.com>
2023-09-26 03:17:46 -0700

    Task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,25 +1,44 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - This prints a listint_t linked list
- * @head: the pointer
+ * free_listint_safe - This function that frees a listint_t list
+ * @h: the pointer
  *
- * Return: nodes
- **/
-size_t print_listint_safe(const listint_t *head)
+ * Return: the size of the list, otherwise 0
+ *
+ */
+
+size_t free_listint_safe(listint_t **h)
 {
-	size_t nodes = 0;
-	const listint_t *aux_node = head;
+	listint_t *node1;
+	listnode_t *nodes = NULL; /* stores address of nodes */
+	size_t nums = 0;
 
-	if (!head)
-		exit(98);
+	if (h == NULL)
+		return (0);
 
-	while (aux_node)
+	/* while you have not ennumsered a loop */
+	while (!is_in_nodes(nodes, *h))
 	{
-		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
-		aux_node = aux_node->next;
-		nodes++;
+		/* check if the malloc fails then exit */
+		if (!add_nodeptr(&nodes, *h))
+		{
+			free_listnode(nodes);
+			exit(98);
+		}
+		node1 = *h;
+		*h = (*h)->next;
+		free(node1);
+		nums++;
 	}
+	/* if you ennumser a loop */
+	if (*h != NULL)
+		*h = NULL;
 
-	return (nodes);
+	/* print where the loop starts */
+	/*	printf("-> [%p] %d\n", (void *)head, head->n); */
+	free_listnode(nodes);
+	/* return number of nodes */
+
+	return (nums);
 }
