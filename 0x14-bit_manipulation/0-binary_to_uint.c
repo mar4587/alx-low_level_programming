c01ee7eda62529321ceb63dc5413e70d179d8b7b
mar4587 <marshetnigatu@yahoo.com>
2023-09-29 05:04:37 -0700

    Task zero

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * binary_to_rent - This converts a binary number to
+ * an unsigned int
+ * @b: pointer
+ *
+ * Return: the unsigned int
+ *
+ */
+unsigned int binary_to_rent(const char *b)
+{
+	unsigned int re;
+	int len, bin;
+
+	if (!b)
+		return (0);
+
+	re = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+	{
+
+	}
+	for (len--, bin = 1; len >= 0; len--, bin *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			re += bin;
+		}
+	}
+	return (re);
+}
