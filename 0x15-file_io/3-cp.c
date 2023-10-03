1e452ccbbe4f23d1e8c901e07b36c990b9d0e988
mar4587 <marshetnigatu@yahoo.com>
2023-10-03 00:07:12 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
-
 /**
- * create_buffer - This allocates 1024 bytes for a buffer
- * @file: The name of the file
+ * create_buffer - This allocates 1024 bytes for a bufferr
+ * @file: the file buffer
  *
- * Return: the buffer
+ * Return: the pointer to buffer
+ *
  */
 char *create_buffer(char *file)
 {
@@ -22,21 +23,22 @@ char *create_buffer(char *file)
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
+
 	return (buffer);
 }
+
 /**
- * close_file - This loses the file descriptor
- * @fd: The file descriptor
+ * close_file - This closes the file descriptors
+ * @fd: the file descriptor
  *
- * Return: void
  */
 void close_file(int fd)
 {
-	int hr;
+	int cl;
 
-	hr = close(fd);
+	cl = close(fd);
 
-	if (hr == -1)
+	if (cl == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -44,41 +46,46 @@ void close_file(int fd)
 }
 
 /**
- * main - This copies the contents of
- * a file to another file
- * @argc: the number of arguments
+ * main - This copies the contents of a file to another file
+ * @argc: The number of arguments
  * @argv: the arguments
  *
  * Return: 0
- * Description: If the argument count is incorrect - exit code 97
+ *
+ * Description: If incorrect - exit code 97
+ *              If cannot be read - exit code 98
+ *              If cannot be created or written to - exit code 99
+ *              If cannot be closed - exit code 100
+ *
  */
 int main(int argc, char *argv[])
 {
-	int sr, to;
-	int v, w;
+	int frm;
+	int to, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_sr file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_frm file_to\n");
 		exit(97);
 	}
 
 	buffer = create_buffer(argv[2]);
-	sr = open(argv[1], O_RDONLY);
-	v = read(sr, buffer, 1024);
+	frm = open(argv[1], O_RDONLY);
+	r = read(frm, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (sr == -1 || v == -1)
+		if (frm == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read sr file %s\n", argv[1]);
+				"Error: Can't read frm file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		w = write(to, buffer, v);
+		w = write(to, buffer, r);
+
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
@@ -87,13 +94,13 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 
-		v = read(sr, buffer, 1024);
+		r = read(frm, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (v > 0);
+	} while (r > 0);
 
 	free(buffer);
-	close_file(sr);
+	close_file(frm);
 	close_file(to);
 	return (0);
 }
