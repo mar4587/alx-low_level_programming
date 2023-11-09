fcc5ec36393b854b19d06064b64675e4d52a057d
mar4587 <marshetnigatu@yahoo.com>
2023-11-09 03:51:47 -0800

    Add node at the end

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - This adds a nw node at the end of a dlistint_t list
+ * @head: the head
+ * @n: the value
+ *
+ * Return: the nw element
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *h;
+	dlistint_t *nw;
+
+	nw = malloc(sizeof(dlistint_t));
+	if (nw == NULL)
+		return (NULL);
+
+	nw->n = n;
+	nw->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = nw;
+	}
+	else
+	{
+		*head = nw;
+	}
+
+	nw->prev = h;
+
+	return (nw);
+}
