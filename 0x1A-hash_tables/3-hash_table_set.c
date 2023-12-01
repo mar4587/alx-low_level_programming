3b176cbb5c5081e7411179b909330eab2e8043f6
mar4587 <marshetnigatu@yahoo.com>
2023-12-01 04:22:11 -0800

    Task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (k = index; ht->array[j]; j++)
+	for (j = index; ht->array[j]; j++)
 	{
 		if (strcmp(ht->array[j]->key, key) == 0)
 		{
